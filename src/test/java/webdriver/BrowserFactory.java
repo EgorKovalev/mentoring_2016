70676885c5bb1f621de0342fb3f6fd582d0ff160
Yahor_Kavaliou <yahor.kavaliou@thomsonreuters.com>
2016-08-08 21:31:50 +0300

    Egor - grid setup

--- src/test/java/webdriver/BrowserFactory.java
+++ src/test/java/webdriver/BrowserFactory.java
@@ -1,41 +1,58 @@
 package webdriver;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import webdriver.Browser.Browsers;
 import javax.naming.NamingException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public abstract class BrowserFactory {
 
     private static RemoteWebDriver setUp(final Browsers type) {
-        DesiredCapabilities capabilitiesProxy = null;
+        DesiredCapabilities capabilitiesProxy = new DesiredCapabilities();
+        capabilitiesProxy.setPlatform(Platform.WINDOWS);
         RemoteWebDriver driver = null;
 
         switch (type) {
             case FIREFOX:
-                driver = new FirefoxDriver(capabilitiesProxy);
+                capabilitiesProxy.setBrowserName("firefox");
+                //driver = new FirefoxDriver(capabilitiesProxy);
                 break;
 
             case CHROME:
+                capabilitiesProxy.setBrowserName("chrome");
                 //TODO: in progress
                 break;
 
             case IEXPLORE:
+                capabilitiesProxy.setBrowserName("IE");
                 //TODO: in progress
                 break;
 
             case OPERA:
+                capabilitiesProxy.setBrowserName("opera");
                 //TODO: in progress
                 break;
 
             case SAFARI:
+                capabilitiesProxy.setBrowserName("safari");
                 //TODO: in progress
                 break;
 
             default:
                 break;
         }
+
+        try {
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilitiesProxy);
+        }
+        catch(MalformedURLException ex){
+            ex.printStackTrace();
+        }
+
         return driver;
     }
 
